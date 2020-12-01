55311679d12c0389bc78fe823df2c6d494bbb774
richard onyett <onyettr@gmail.com>
2020-12-01 09:13:09 -0800

    fixed bar to 100%

--- progress.c
+++ progress.c
@@ -2,39 +2,97 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int barlength = 60;
-static char bar[10];
+#define BAR_WIDTH       80
 
-void c_progress(char label[], int step, int total)
+void PrintProgressBar(int CurrSize, int MaxSize)
 {
-  int width = barlength - strlen(label);
-  int pos = (step * width) / total;
-  int percent;
-  int i;
+    static char ProgressBar[BAR_WIDTH+1];
+    static char OutputStr[BAR_WIDTH*2];    
+    int Progress;
+    int HashCount;
 
-  percent = (step * 100) / total;
+    memset(OutputStr, ' ', sizeof(OutputStr));    
+    memset(ProgressBar, ' ', sizeof(ProgressBar));
+    ProgressBar[BAR_WIDTH] = ']';
+    ProgressBar[BAR_WIDTH+1]   = '\0';        
+    OutputStr[BAR_WIDTH] = '\0';
+      
+    if (MaxSize <= 0)
+    {
+      printf("\n");
 
-  printf("%s [", label);
-  for (i=0; i < pos; i++)
-  {
-    printf("%c", '#');
-  }
-  // printf("% *c", width - pos + 1, ']');
-  
-  printf("] %d%% \r", percent);
-  (void)fflush(stdout);  
+      return;
+    }
+
+    Progress  = (int)((CurrSize / (float)(MaxSize)) * 100);
+
+    HashCount = (CurrSize * BAR_WIDTH) / MaxSize; 
+
+    ProgressBar[0] = '[';
+
+    for (int i=1; i < HashCount; i++)    
+    {
+      ProgressBar[i] = '#';
+    }
+
+    sprintf(OutputStr,"%s %3d%%: %6d/%6d Blocks", ProgressBar, Progress, CurrSize, MaxSize);
+    printf("%s", OutputStr);
+
+    for(int i=0; i < sizeof(OutputStr); i++)
+    {
+      printf("\b");
+    }
+    (void)fflush(stdout);
+
+    if (Progress >= 100)
+    {
+      printf("\n");
+    }
 }
 
 int main ( void )
 {
   int i;
-  
-  memset(bar, '-', (size_t)barlength);
-  bar[barlength-1] = '\0';
 
-  for (i=0; i < 100; i++)
+  for (i=0; i <= 13967; i++)
   {
-    c_progress("Test", i, 100);
-    for(int j=0; j < 100000000; j++);
+    PrintProgressBar(i,13967);
+    for(int j=0; j < 100000; j++);    // Fake delay()
+  }
+
+  for (i=0; i <= 900; i++)
+  {
+    PrintProgressBar(i,900);
+    for(int j=0; j < 100000; j++);    // Fake delay()
+  }
+  
+  for (i=0; i <= 50; i++)
+  {
+    PrintProgressBar(i,50);
+    for(int j=0; j < 100000000; j++);    // Fake delay()
+  }
+  
+  for (i=0; i <= 10; i++)
+  {
+    PrintProgressBar(i,10);
+    for(int j=0; j < 100000000; j++);    // Fake delay()
+  }
+
+  for (i=0; i <= 4; i++)
+  {
+    PrintProgressBar(i,4);
+    for(int j=0; j < 100000000; j++);    // Fake delay()
+  }
+
+  for (i=0; i <= 100; i++)
+  {
+    PrintProgressBar(i,100);
+    for(int j=0; j < 10000000; j++);    // Fake delay()
+  }
+  
+  for (i=0; i <= 200; i++)
+  {
+    PrintProgressBar(i,200);
+    for(int j=0; j < 10000; j++);    // Fake delay()
   }
 }
